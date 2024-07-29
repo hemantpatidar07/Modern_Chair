85898b670dff0b4d5d0f8ad109380a5ec36c9ba2
hemantpatidar07 <patidarhemant1990@gmail.com>
2024-07-29 16:10:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+// apply eventlistner
+document.addEventListener("click",function (){
+    console.log("I clicked the page");
+});  //I clicked the page
+
+// //on web site
+let gaurav = document.querySelector(".title-up");
+
+gaurav.addEventListener("click",function () {
+     gaurav.style.background = "red";
+    // console.log("hemant");
+});
